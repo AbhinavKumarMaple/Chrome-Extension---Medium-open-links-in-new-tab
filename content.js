41f534b2d7626d33d53dc8be20facbd572d40638
Abhinav Kumar <virusfuf@gmail.com>
2023-12-29 02:27:47 +0530

    added class base filter

--- content.js
+++ content.js
@@ -1,17 +1,18 @@
-console.log("your extension is working");
+console.log("Your extension is working");
 
 // content.js
 window.onload = function () {
-  // Select all anchor (a) elements on the page
+  // Select all anchor (a) elements inside the div with class "dx s"
+  const container = document.querySelector(".dx.s");
+  if (container) {
+    container.querySelectorAll("a").forEach((link) => {
+      link.addEventListener("click", (event) => {
+        // Open the link in a new tab
+        window.open(link.href, "_blank");
 
-  // Loop through each link and add a click event listener
-  document.querySelectorAll("a").forEach((link) => {
-    link.addEventListener("click", (event) => {
-      // Open the link in a new tab
-      window.open(link.href, "_blank");
-
-      // Prevent the default link behavior (opening in the same tab)
-      event.preventDefault();
+        // Prevent the default link behavior (opening in the same tab)
+        event.preventDefault();
+      });
     });
-  });
+  }
 };
